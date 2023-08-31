574f888184a144f4b812d10ebabd97cfb4b08beb
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-31 15:14:24 +0530

    add the main logic in simple format.

---
+++ main.py
@@ -0,0 +1,6 @@
+base_url = "https://vipergirls.to/threads/5842541-VlXEN-Photo-Collection"
+# save from base url
+current_page = 2
+new_url = f"{base_url}/?page={current_page}"
+#save data from current url
+# repeat until meet end
