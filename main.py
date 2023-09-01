e8e5f8967bc79edb9779c5e72bc5cb79a1aa3224
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-01 19:08:38 +0530

    switch two values.

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ while next_page_url:
         for post in soup.select("#postlist > ol#posts > li.postcontainer blockquote.postcontent.restore")
         if post.select_one("div > font") and post.select("a > img")
     ]
-    sql = "INSERT INTO images_from_url (url, title, preview_image_link, high_resolution_image_link) " \
+    sql = "INSERT INTO images_from_url (url, title, high_resolution_image_link, preview_image_link) " \
           "VALUES (%s, %s, %s, %s)"
     next_page_url_tag = soup.select_one("#pagination_bottom .prev_next > a[title~='Next']")
     if next_page_url_tag:
